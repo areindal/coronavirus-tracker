f21718338338abba241d50e525093535260d0579
Rein Dalmases <alvaro.rein-dalmases@capgemini.com>
2020-03-27 19:40:35 +0100

    Change csv url adress

--- src/main/java/io/javabrains/coronavirustracker/services/CoronaVirusDataService.java
+++ src/main/java/io/javabrains/coronavirustracker/services/CoronaVirusDataService.java
@@ -24,7 +24,7 @@ import io.javabrains.coronavirustracker.models.LocationStats;
 @Service
 public class CoronaVirusDataService {
 
-    private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
+    private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
     private List<LocationStats> allStats = new ArrayList<>();
     private static final Logger LOGGER = LoggerFactory.getLogger(CoronaVirusDataService.class);
